f4bfbfd3dcf41d4d1830d66f1148e76b82ea1364
indupriya968 <yarramaneniindu@gmail.com>
2022-09-07 19:38:23 +0530

    comments added in loginuser

--- loginuser.cs
+++ loginuser.cs
@@ -2,7 +2,7 @@
 {
     public class loginuser
     {
-        public string? username { get; set; }
+        public string? username { get; set; } // username
         public string? password { get; set; }
     }
 }
